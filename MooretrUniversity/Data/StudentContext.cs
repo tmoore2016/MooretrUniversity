30aca1a8c60a3e20c582696aa47bc6d9261d076f
T Moore <tb0moore@gmail.com>
2022-03-09 12:11:51 -0700

    Scaffold Student model to Razor CRUD pages

---
+++ MooretrUniversity/Data/StudentContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoortrUniversity.Models;
+
+namespace MooretrUniversity.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext (DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoortrUniversity.Models.Student> Student { get; set; }
+    }
+}
