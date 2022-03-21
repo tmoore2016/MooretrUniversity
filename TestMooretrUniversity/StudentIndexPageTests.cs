62aea6fcbc0c7e73d4891f1d8e19a1da3d40fae5
T Moore <tb0moore@gmail.com>
2022-03-21 06:49:47 -0600

    Install xunit and add dependency to project

---
+++ TestMooretrUniversity/StudentIndexPageTests.cs
@@ -0,0 +1,14 @@
+using MooretrUniversity.Pages.Students;
+using Xunit;
+
+namespace TestMooretrUniversity;
+
+public class StudentIndexPageTests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
+
