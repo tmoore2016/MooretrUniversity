52f39862da14ac73d6e2269c3623a697c8ccd433
T Moore <tb0moore@gmail.com>
2022-03-30 12:35:24 -0600

    Fix link to Edit, Details, Delete pages for Instructors

---
+++ MooretrUniversity/Models/SchoolViewModels/AssignedCourseData.cs
@@ -0,0 +1,10 @@
+﻿namespace MooretrUniversity.Models.SchoolViewModels
+{
+    // Class to track courses assigned to an instructor, used for the Instructor page checkboxes
+    public class AssignedCourseData
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
